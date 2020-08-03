4b13db93761b68bd30f579952d46bef48d7c6908
Mehmet <alkanchosen1@gmail.com>
2020-08-03 13:50:44 +0300

    - Minor fixes

--- src/components/AdminPanel/ListCategories.js
+++ src/components/AdminPanel/ListCategories.js
@@ -2,17 +2,9 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import DashboardIcon from '@material-ui/icons/Dashboard';
 import EventIcon from '@material-ui/icons/Event';
-import BarChartIcon from '@material-ui/icons/BarChart';
 import {Link} from "react-router-dom";
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import AddEventDialog from "./AddEventDialog";
-import ParticipantCountGraphContainer from "./ParticipantCountGraph/ParticipantCountGraphContainer";
-
-function ListItemLink(props) {
-    return <ListItem button component="a" {...props} />;
-}
 
 export const mainListItems = (
     <div>
--- src/components/AdminPanel/ParticipantCountGraph/ParticipantCountGraphComponent.js
+++ src/components/AdminPanel/ParticipantCountGraph/ParticipantCountGraphComponent.js
@@ -1,32 +1,8 @@
-import React, {PureComponent, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import {
-    BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Label, LabelList
+    BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, LabelList
 } from 'recharts';
 import axios from "axios";
-
-const data = [
-    {
-        name: 'Page A', uv: 4000, pv: 2400
-    },
-    {
-        name: 'Page B', uv: 3000, pv: 1398
-    },
-    {
-        name: 'Page C', uv: 2000, pv: 9800
-    },
-    {
-        name: 'Page D', uv: 2780, pv: 3908
-    },
-    {
-        name: 'Page E', uv: 1890, pv: 4800
-    },
-    {
-        name: 'Page F', uv: 2390, pv: 3800
-    },
-    {
-        name: 'Page G', uv: 3490, pv: 4300
-    },
-];
 
 export default function ParticipantCountGraphComponent(props) {
     const [events, setEvents] = React.useState([])
